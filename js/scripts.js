a783e5f199ae3eb164f6624cc2af5f0e9241649b
Brian Bauska <brianbauska@gmail.com>
2025-11-11 16:20:31 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -121,7 +121,7 @@ function switchToNextVideo() {
     currentVideoIndex = (currentVideoIndex + 1) % videoIds.length;
     iframe.src = `https://www.youtube.com/embed/${videoIds[currentVideoIndex]}?autoplay=1&controls=0&loop=1&mute=1`;
     snowEffect.style.opacity = 0;
-  }, 4000); // 2 seconds of static before switching
+  }, 4000);  /* 4 seconds of static before switching */
 }
 https://youtu.be/
 iframe.addEventListener("load", () => {
